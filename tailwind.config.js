d912af700867122b00d6286f4b698101d059f2de
javierlnc <javierlnc@icloud.com>
2024-11-26 21:52:41 -0500

    add new color to tailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,16 @@ module.exports = {
       current: 'currentColor',
       'white': '#ffffff',
       'orange' : {
+        50:'#F2B591',
         100:'#D97E4A',
         300:'#B4653C'
+
       },
       'stone' :{
         900:'#261B0E'
       },
       gray: colors.gray,
+      yellow: colors.yellow
 
     },
     extend: {},
