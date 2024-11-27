455940afd20ecae0c96afaab8c38afd530ebce37
javierlnc <javierlnc@icloud.com>
2024-11-26 20:23:49 -0500

    add toggle response and change color for tailwind

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  isMenuOpen = false;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    colors:{
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'orange' : {
+        100:'#D97E4A',
+        300:'#B4653C'
+      },
+      'stone' :{
+        900:'#261B0E'
+      },
+      gray: colors.gray,
+
+    },
     extend: {},
   },
   plugins: [],
