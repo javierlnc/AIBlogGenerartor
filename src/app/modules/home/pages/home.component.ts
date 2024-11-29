09a755f823666b56d8fedb8ee55c075fe3b675a2
javierlnc <javierlnc@icloud.com>
2024-11-28 19:02:40 -0500

    Remove innecesary import

--- src/app/modules/home/pages/home.component.ts
+++ src/app/modules/home/pages/home.component.ts
@@ -5,7 +5,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
