434e5a189e8f397fa41fb5cf23bb24294aa3faee
javierlnc <javierlnc@icloud.com>
2024-11-28 19:01:34 -0500

    add navigation in button and navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from '@modules/pages/home/home.component';
+import { HomeComponent } from '@modules/home/pages/home.component';
 
 export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'signin',
+    loadComponent: () =>
+      import('@modules/sign-in/sign-in.component').then(
+        (c) => c.SignInComponent
+      ),
   },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
--- src/app/modules/home/pages/home.component.ts
+++ src/app/modules/home/pages/home.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
